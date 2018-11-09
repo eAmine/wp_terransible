cc46a2c93dee03ba30a817e5b8ba54d8fa1a939e
root <root@eamine2.mylabserver.com>
2018-11-09 08:11:49 +0000

    Almost ready

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  version = "1.13.0"
   region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
@@ -207,28 +208,282 @@ resource "aws_route_table_association" "wp_private2_assoc" {
 
 # Security groups
 
-resource "aws_security_group" "wp_dev_sg" { 
- name="wp_dev_sg"
- description="Used for access to the dev instance"
- vpc_id ="${aws_vpc.wp_vpc.id}"
- #SSH
- ingress {
-  from_port    = 22
-  to_port      = 22
-  protocol     = "tcp"
-  cidr_blocks  = ["${var.localip}"]
- }
- #HTTP
- ingress {
-  from_port    = 80
-  to_port      =  80
-  protocol     = "tcp"
-  cidr_blocks  = ["${var.localip}"]
- }
- egress{
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
- }
-} 
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
+  description = "Used for access to the dev instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Public Security groups
+resource "aws_security_group" "wp_public_sg" {
+  name        = "wp_public_sg"
+  description = "Used for access to the public instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Privtae Security groups
+resource "aws_security_group" "wp_private_sg" {
+  name        = "wp_private_sg"
+  description = "Used for access to the private instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #HTTP
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#RDS Security Group
+resource "aws_security_group" "wp_rds_sg" {
+  name        = "wp_rds_sg"
+  description = "used for RDS instances"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #SQL access from public/private security groups
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+      "${aws_security_group.wp_private_sg.id}",
+    ]
+  }
+}
+
+#VPC Endpoint for S3
+
+resource "aws_vpc_endpoint" "wp_private-s3_endpoint" {
+  vpc_id       = "${aws_vpc.wp_vpc.id}"
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+
+  route_table_ids = ["${aws_vpc.wp_vpc.main_route_table_id}",
+    "${aws_route_table.wp_public_rt.id}",
+  ]
+
+  policy = <<POLICY
+{ 
+    "Statement": [
+      { 
+        "Action": "*",
+        "Effect": "Allow",
+        "Resource": "*",
+        "Principal":"*"
+      }
+    ]
+}
+POLICY
+}
+
+#------ S3 code bucket--------
+
+resource "random_id" "wp_code_bucket" {
+  byte_length = 2
+}
+
+resource "aws_s3_bucket" "code" {
+  bucket        = "${var.domain_name}-${random_id.wp_code_bucket.dec}"
+  acl           = "private"
+  force_destroy = true
+
+  tags {
+    Name = "code bucket"
+  }
+}
+
+#---------- RDS--------------
+resource "aws_db_instance" "wp_db" {
+  allocated_storage      = 10
+  engine                 = "mysql"
+  engine_version         = "5.6.27"
+  instance_class         = "${var.db_instance_class}"
+  name                   = "${var.dbname}"
+  username               = "${var.dbpassword}"
+  db_subnet_group_name   = "${aws_db_subnet_group.wp_rds_subnetgroup.name}"
+  vpc_security_group_ids = ["${aws_security_group.wp_rds_sg.id}"]
+  skip_final_snapshot    = true
+}
+
+#----------- Load Balancer------------
+resource "aws_elb" "wp_elb" {
+  name = "${var.domain_name}-elb"
+
+  subnets = ["${aws_subnet.wp_public1_subnet.id}",
+    "${aws_subnet.wp_public2_subnet.id}",
+  ]
+
+  security_groups = ["${aws_security_group.wp_public_sg.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = "${var.elb_healthy_threshold}"
+    unhealthy_threshold = "${var.elb_unhealthy_threshold}"
+    timeout             = "${var.elb_timeout}"
+    target              = "TCP:80"
+    interval            = "${var.elb_interval}"
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "wp_${var.domain_name}-elb"
+  }
+}
+
+#------------- DEV Instance------------------
+#key pair
+resource "aws_key_pair" "wp_auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+resource "aws_instance" "wp_dev" {
+  instance_type = "${var.dev_instance_type}"
+  ami           = "${var.dev_instance_type}"
+
+  tags {
+    name = "wp_dev"
+  }
+
+  key_name               = "${aws_key_pair.wp_auth.id}"
+  vpc_security_group_ids = ["${aws_security_group.wp_dev_sg.id}"]
+  iam_instance_profile   = "${aws_iam_instance_profile.s3_access_profile.id}"
+  subnet_id              = "${aws_subnet.wp_public1_subnet.id}"
+
+  provisioner "local-exec" {
+    command = <<EOD
+cat <<EOF > aws_hosts
+[dev]
+${aws_instance.wp_dev.public.ip}
+[dev:vars]
+s3code=${aws_s3_bucket.code.bucket}
+domain=${var.domain_name}
+EOF
+EOD
+  }
+
+  provisioner "local-exec" {
+    command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.wp_dev.id} --profile superman && ansible-playbook -i aws_hots wordpress.yml"
+  }
+}
+
+#-------------- Golden AMI----------------
+
+# random ami id
+resource "random_id" "golden_ami" {
+  byte_length = 3
+}
+
+resource "aws_ami_from_instance" "wp_golden" {
+  name               = "wp_ami-${random_id.golden_ami.b64}"
+  source_instance_id = "${aws_instance.wp_dev.id}"
+
+  provisioner "local-exec" {
+    command = <<EOT
+cat <<EOF > userdata
+#!/bin/bash
+/usr/bin/aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/
+/bin/touch /var/spool/cron/root
+sudo /bin/echo '*/5 * * * * aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html' >> /var/spool/cron/root
+EOF
+EOT
+  }
+}
+
+#---------------- launch configuration----------------
+
+resource "aws_launch_configuration" "wp_lc"{
+  name_prefix="wp_lc-"
+  image_id="${aws_ami_from_instance.wp_golden.id}"
+  instance_type="${var.lc_instance_type}"
+  security_groups=["${aws_security_group.wp_private_sg.id}"]
+  iam_instance_profile ="${aws_iam_instance_profile.s3_access_profile.id}"
+  key_name= "${aws_key_pair.wp_auth.id}"
+  user_data= "${file("userdata")}"
+  lifecycle{
+    create_before_destroy = true
+  }
+}
+
+#--------------- ASG ----------------
+resource "aws_autoscaling_group" "wp_asg"{
+  name ="asg-${aws_launch_configuration.wp_lc.id}"
+  max_size="${var.asg_max}"
+  min_size ="${var.asg_min}"
+  health_check_grace_period="${var.asg_grace}"
+  health_check_type="${var.asg_hct}"
+  desired_capacity= "${var.asg_cap}"
+  force_delete = true
+  load_balancers = ["${aws_elb.wp_elb.id}"]
+  
+  vpc_zone_identifier=["${aws_subnet.wp_private1_subnet.id}","${aws_subnet.wp_private2_subnet.id}"]
+  launch_configuration="${aws_launch_configuration.wp_lc.name}"
+  tag {
+    key="Name"
+    value="wp_asg-instance"
+    propagate_at_launch = true
+  }
+  
+  lifecycle{
+    create_before_destroy = true
+  }
+}
